e1b0940848b6dbd3bfa76dfd50c3c1940fdcccd5
petrec <constantinpetre89@gmail.com>
2018-04-29 11:44:06 +0300

    class Catalog

---
+++ src/sda/calalogdigital/Catalog.java
@@ -0,0 +1,20 @@
+package sda.calalogdigital;
+
+public class Catalog {
+
+    private Student student;
+    private Subject subject;
+    private Grade grade;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+}
