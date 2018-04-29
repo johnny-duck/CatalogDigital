1a2557e7369f251fd62cffd8625120a4deb6ad7c
Daria Timofte <daria_ana99@yahoo.com>
2018-04-29 11:48:33 +0300

    Issue 3

---
+++ src/sda/calalogdigital/Subject.java
@@ -0,0 +1,40 @@
+package sda.calalogdigital;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Subject {
+
+    String name;
+    List<Grade> grade;
+
+    public Subject(String name)
+    {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Grade> getGrade() {
+        return grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return Objects.equals(name, subject.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
+    }
+    
+
+}
